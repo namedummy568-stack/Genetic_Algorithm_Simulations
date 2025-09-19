614c8121b2b273556d104cdfba26fbc460e7f4d7
namedummy568-stack <namedummy568@gmail.com>
2025-09-19 15:34:30 +0530

    Bug fix: off-by-one error in population generation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 print('Genetic Algorithm Simulation')
 
+def initialize_population(size):
+    # Bug fix: ensure population size is correct
+    return [[] for _ in range(size)]
+
 def selection():
     pass
 
